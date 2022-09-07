54be4a439d394abd33581bb4473f1b622360b622
rah536 <raherrera536@gmail.com>
2022-09-06 21:26:27 -0300

    Prueba push desde VS

--- WindowsFormsApp1/Menu.cs
+++ WindowsFormsApp1/Menu.cs
@@ -21,6 +21,7 @@ namespace WindowsFormsApp1
 
         private void Menu_Load(object sender, EventArgs e)
         {
+            //boton Ver Listado
             ArticuloNegocio negocio = new ArticuloNegocio();
             dgvArticulos.DataSource = negocio.listar();
            
